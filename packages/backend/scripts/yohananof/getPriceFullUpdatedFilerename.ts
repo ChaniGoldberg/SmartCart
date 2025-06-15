f7c952cbc07f017f166ff35b7026a338224534b3
הודיה טולדנו <a0547006431@gmail.com>
2025-06-15 22:04:03 +0300

    commit 15.06.25 22:04

--- packages/backend/scripts/yohananof/getPriceFullUpdatedFilerename.ts
+++ packages/backend/scripts/yohananof/getPriceFullUpdatedFilerename.ts
@@ -1,18 +1,17 @@
 export async function getUpdatedPriceFullByStoreId(allFiles: string[], storeId: string): Promise<string> {
     const filterPromo: string[] = allFiles.filter(x => x.startsWith('PriceFull'))
-    const dates: string[] = []
+    const matchingFiles: string[] = []
     for (let i = 0; i < filterPromo.length; i++) {
         
         const filename = filterPromo[i]
       
         const parts = filename.split('-');
-        const dateWithExt = parts[2];
-        const data = dateWithExt.split('.')[0];
+        
         const id=parts[1];
         if(id==storeId)
-       { dates.push(data)}
+       { matchingFiles.push(filename)}
     }
-    dates.sort()
+    matchingFiles.sort()
 
-    return dates[dates.length - 1]  
+    return matchingFiles[matchingFiles.length - 1]  
 }
