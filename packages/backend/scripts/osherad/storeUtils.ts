e9598bd936256687b01fea4986fc73f7263de972
talya2005 <talya91262@gmail.com>
2025-06-16 12:27:57 +0300

    getLatestStoreFile #37

--- packages/backend/scripts/osherad/storeUtils.ts
+++ packages/backend/scripts/osherad/storeUtils.ts
@@ -1,6 +1,5 @@
-
-
 export function getLatestStoreFile(fileNames: string[]): string | null {
+  
   fileNames.sort((a,b)=>{
     const timeA = a.match(/-(\d{12})\.xml$/)?.[1] ?? '';
     const timeB = b.match(/-(\d{12})\.xml$/)?.[1] ?? '';
