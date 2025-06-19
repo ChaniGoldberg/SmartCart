4b074477133205772624f8444c6119b632ba2b04
yaelJulyard <y0556752591@gmail.com>
2025-06-19 17:02:45 +0300

    fix feature#8

--- packages/backend/scripts/osherad/PriceUtils.ts
+++ packages/backend/scripts/osherad/PriceUtils.ts
@@ -6,22 +6,21 @@ export type PriceFile = {
 };
 export function getLatestPriceFilesPerStore(fileNames: string[]): string[] {
     const latestByStore: Record<string, PriceFile> = {};
-    fileNames.forEach(fileName => {
+    fileNames.forEach(url => {
+        // חילוץ שם הקובץ מתוך הקישור
+        const fileName = url.substring(url.lastIndexOf('/') + 1);
+
         if (!fileName.startsWith('PriceFull')) return;
-        const match = fileName.match(/^([A-Za-z]+)\d+-([0-9]{3})-([0-9]{12})$/);
+        // התאמה עם התבנית של שם הקובץ
+        const match = fileName.match(/^([A-Za-z]+)\d+-([0-9]{3})-([0-9]{12})(?:\.gz)?$/);
         if (!match) return;
+
         const [, type, store, timestamp] = match;
-        if (
-            !latestByStore[store] ||
-            timestamp > latestByStore[store].timestamp
-        ) {
-            latestByStore[store] = {
-                fileName,
-                timestamp,
-                store,
-                type
-            };
+        
+
+        if (!latestByStore[store] || timestamp > latestByStore[store].timestamp) {
+            latestByStore[store] = { fileName, timestamp, store, type };
         }
     });
     return Object.values(latestByStore).map(entry => entry.fileName);
-}
+    };
