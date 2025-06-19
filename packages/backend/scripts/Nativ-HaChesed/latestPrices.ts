be879a2860be94d418f6f131e0c577dce1eedef0
yaffy aiger <yafa6105@gmail.com>
2025-06-19 15:13:10 +0300

    change the func to use with sort

--- packages/backend/scripts/Nativ-HaChesed/latestPrices.ts
+++ packages/backend/scripts/Nativ-HaChesed/latestPrices.ts
@@ -1,14 +1,11 @@
 export function getLatestUpdatePriceFullFile(strings: string[], branch: string): string | null {
 
-    const arr = strings.filter(str => str.startsWith("PriceFull") && str.includes(`-${branch}-`) && str.match(/(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})$/));
-    arr.sort((a, b) => {
-    ;
-        const dateA =a.slice(-12);// חותך את 12 התווים האחרונים וממיר לתאריך
-        const dateB = b.slice(-12); // חותך את 12 התווים האחרונים וממיר לתאריך
-        return +dateB - +dateA; // מחזיר את תוצאת ההשוואה
-    })
+    const arr = strings
+        .filter(str => str.startsWith("PriceFull") && str.includes(`-${branch}-`))
+        .sort()
+        .reverse();
 
 
-    return arr[0]?arr[0]:null;
+    return arr[0] ? arr[0] : null;
 };
 
