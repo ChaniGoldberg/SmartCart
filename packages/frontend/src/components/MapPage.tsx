a1d98f69fa5593b7596d5db6609e47a2fab34862
yaelJulyard <y0556752591@gmail.com>
2025-06-23 15:07:49 +0300

    component create map

---
+++ packages/frontend/src/components/MapPage.tsx
@@ -0,0 +1,16 @@
+
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+export default function MapPage() {
+  return (
+    <div style={{ height: '100vh', width: '100%' }}>
+      <MapContainer center={[32.0853, 34.7818]} zoom={13} style={{ height: '100%', width: '100%' }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  );
+}
--- packages/frontend/src/routes/Routing.tsx
+++ packages/frontend/src/routes/Routing.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "../pages/Home"
+import MapPage from "../components/MapPage"
 
 export const Routing=()=>{
     return(
     <Routes>
 <Route path="/" element={<Home/>} ></Route>
+<Route path="/map" element={<MapPage/>} ></Route>
     </Routes>
     )
 }
