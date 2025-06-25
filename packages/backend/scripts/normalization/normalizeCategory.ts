3c3019215e31383db7a91ee81bc0db0fd0335d21
Chana Alter <chana.15590@gmail.com>
2025-06-25 14:09:03 +0300

    feature_#104 fixed

---
+++ packages/backend/scripts/normalization/normalizeCategory.ts
@@ -0,0 +1,39 @@
+import { categories } from "@smartcart/shared/src/categories"
+import { Price } from "@smartcart/shared/src/prices"
+
+async function getCategoryFromGpt(product: string): Promise<string> {
+
+    const categoryUrl = `http://localhost:8080/Category/${encodeURIComponent(product)}`;
+    const categoryResponse = await fetch(categoryUrl);
+
+    if (!categoryResponse.ok) {
+        throw new Error(`Error fetching category: ${categoryResponse.statusText}`);
+    }
+
+    const category = await categoryResponse.text();
+    return category;
+}
+
+function checkCategoryInDB(categoryName: string): boolean {
+    for (const category of categories) {
+        if (category.name === categoryName) {
+            console.log(`הקטגוריה ${categoryName} קיימת ב-DB`);
+            return true;
+        }
+    }
+    return false;
+}
+
+function addCategory(categoryName: string): void {
+    console.log(`הקטגוריה ${categoryName} לא קיימת ב-DB, מוסיפים אותה`);
+}
+
+export default async function categorizeProductByGPT(product: Price): Promise<string> {
+
+    const categoryOfProduct = await getCategoryFromGpt(product.ItemName);
+    if (!checkCategoryInDB(categoryOfProduct)) {
+        addCategory(categoryOfProduct);
+    }
+
+    return categoryOfProduct;
+}
