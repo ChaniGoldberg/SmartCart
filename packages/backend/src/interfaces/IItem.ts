f01e02373223556f26145a369e02a2a8e597e43f
ChayaStraus <chaya280905@gmail.com>
2025-06-25 14:49:48 +0300

    #233:I changed the name of function

--- packages/backend/src/interfaces/IItem.ts
+++ packages/backend/src/interfaces/IItem.ts
@@ -3,7 +3,7 @@ import { Price } from "@smartcart/shared/src/prices"
 export interface IItem {
 
     getAllItem(): Promise<Price[]>
-    getItemByItemCode(itemCode: string): Promise<Price[] | null>
+    getItemById(itemCode: string): Promise<Price[] | null>
     addItem(item: Price): Promise<void>
     updateItem(item: Price): Promise<void>
     
