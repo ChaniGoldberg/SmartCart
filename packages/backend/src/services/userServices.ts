0445d0cb881bfdecbc99bfb809d523d9efc018c7
yaffy aiger <yafa6105@gmail.com>
2025-06-25 13:21:01 +0300

    fix conflict?

--- packages/backend/src/services/userServices.ts
+++ packages/backend/src/services/userServices.ts
@@ -2,7 +2,6 @@ import jwt from 'jsonwebtoken';
 import { User } from '../../../shared/src/user'
 
 const SECRET_KEY = process.env.JWT_SECRET || 'your-secret-key';
-
 export function createUserTokenByJWT(user: User): string {
 
     return jwt.sign(user, SECRET_KEY);
