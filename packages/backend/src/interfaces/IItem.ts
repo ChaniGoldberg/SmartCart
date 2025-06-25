d9ace29ea0770aebf11d923d968516bd1c68555a
ChayaStraus <chaya280905@gmail.com>
2025-06-25 10:23:25 +0300

    #233:I changed the IItem

--- packages/backend/src/interfaces/IItem.ts
+++ packages/backend/src/interfaces/IItem.ts
@@ -2,8 +2,8 @@ import { Price } from "@smartcart/shared/src/prices"
 
 export interface IItem {
 
-    getAllItem(): Promise<Price>
-    getItemByItemCode(itemCode: number): Promise<Price | null>
+    getAllItem(): Promise<Price[]>
+    getItemByItemCode(itemCode: string): Promise<Price[] | null>
     addItem(item: Price): Promise<void>
     updateItem(item: Price): Promise<void>
     
