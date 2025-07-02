b050f8180e0bf8a6e5ca98d211a26087bcea30fd
ChaniGoldberg <clg33102@gmail.com>
2025-07-02 11:15:16 +0300

    added interface&service for Item

---
+++ packages/backend/src/injection.config.ts
@@ -0,0 +1,3 @@
+import { IItem } from "./interfaces/IItem";
+import { ItemService } from "./services/itemService"; // המוק
+export const itemService: IItem = new ItemService();
---
+++ packages/backend/src/services/itemService.ts
@@ -0,0 +1,28 @@
+import { IItem } from "../interfaces/IItem";
+import { Item } from "@smartcart/shared/src/item";
+import { db } from "../db/dbProvider";
+
+export class ItemService implements IItem {
+    private db: typeof db;
+
+    constructor() {
+        this.db = db;
+    }
+
+    async getAllItem(): Promise<Item[]> {
+        return this.db.Item;
+    }
+
+async getItemById(itemCode: string): Promise<Item | null> {
+    const foundItem = this.db.Item.find((item: Item) => item.ItemCode === itemCode);
+    return foundItem ? foundItem : null;
+}
+
+    async addItem(item: Item): Promise<void> {
+    }
+
+    async updateItem(item: Item): Promise<void> {
+    }
+}
+
+export default ItemService;
