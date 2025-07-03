3f59d52d75382d2fcc7939764cc02d10c4c0a335
ChayaFertig <chayal7217@gmail.com>
2025-07-03 11:18:47 +0300

    build controller to tag

---
+++ packages/backend/src/controllers/tagController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { tagService } from '../services/tagServices';
+
+export const addTag = async (req: Request, res: Response) => {
+    const { tagName } = req.body;
+    try {
+        const newTag = await tagService.addTag(tagName);
+        res.status(201).json({
+            success: true,
+            message: `Tag ${tagName} added successfully!`,
+            timestamp: new Date().toISOString(),
+            tag: newTag // הוסף את התג שנוסף בתגובה
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Error adding tag', error });
+    }
+};
