2b2e2d31a04174ecb873d268118ed2669d35d7fe
miri-farkash-git <miri0534185556@gmail.com>
2025-07-03 10:33:37 +0300

    add and fix TotaqlPrice component

--- packages/frontend/src/components/TotalPrice.tsx
+++ packages/frontend/src/components/TotalPrice.tsx
@@ -4,26 +4,12 @@ import {cartContext} from '../context/cartContext';
 
 const TotalPrice: React.FC = () => {
     const context = useContext(cartContext); // Replace with your actual context if needed
-    function sumTotalPrice() {
-      // This function will sum the total price of all items
-      return context.cartItems.reduce((total, item) => {
-        return total + (item.ItemPrice * item.Quantity);
-      }, 0);
-    }
-    const total = sumTotalPrice();
     
     return (
-      <div style={{
-                  marginLeft: '40%',
-                  width:'20%',
-                  padding: '8px 16px',
-                  backgroundColor: 'green',
-                  color: 'white',
-                  border: 'none',
-                  borderRadius: '4px',
-                }}>
-        Total Price: {total}
-      </div>
+      <div>
+         {context && context.cartItems.reduce((total, item) => {
+          return total + (item.ItemPrice * item.Quantity);  },0)}   
+     </div>
     );
 };
 
