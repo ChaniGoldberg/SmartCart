93507f7a70e221d92c2dd2a3cfa41e8d38ced41c
ChayaFertig <chayal7217@gmaul.com>
2025-07-10 15:20:56 +0300

    update tagRoutes

--- packages/backend/src/routes/tagRoutes.ts
+++ packages/backend/src/routes/tagRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { addTag, getAllTags } from '../controllers/tagController';
-
+import { addTag, getAllTags, addNewTagsToItems } from '../controllers/tagController'; 
 const router = Router();
+
 router.get('/tags', getAllTags); 
 router.post('/addtag/:tagName', addTag);
+router.post('/addtags-to-items', addNewTagsToItems); 
 
 export default router;
