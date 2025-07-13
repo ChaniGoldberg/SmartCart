e194e351ee2b0739e78c792a45189a94baf78977
yaffy aiger <yafa6105@gmail.com>
2025-07-13 10:34:58 +0300

    fix route to login

--- packages/frontend/src/routes/Routing.tsx
+++ packages/frontend/src/routes/Routing.tsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "../pages/Home"
 import MapPage from "../components/MapPage";
-import Login from "../pages/Login";
+// import Login from "../pages/Login";
 import Cart from "../components/Cart"
+import AuthForm from "../components/authForm";
 
 export const routes = [
     { path: "/", element: <Home />, label: "בית" },
     { path: "/map", element: <MapPage />, label: "מפה" },
-     { path: "/login", element: <Login />, label: "התחברות" },
+     { path: "/login", element: <AuthForm onClose={() => {true}}/>, label: "התחברות" },
      { path: "/cart", element: <Cart />, label: "סל" },
 
 ];
