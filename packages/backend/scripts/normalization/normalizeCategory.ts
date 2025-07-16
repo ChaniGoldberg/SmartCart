c95dd8f3027cb7677be00831d7d37c8f13fb5799
Chana Alter <chana.15590@gmail.com>
2025-07-16 10:30:26 +0300

    without link

--- packages/backend/scripts/normalization/normalizeCategory.ts
+++ packages/backend/scripts/normalization/normalizeCategory.ts
@@ -2,16 +2,22 @@ import { Tag } from "@smartcart/shared/src/tag";
 import "../../mockData/tags.json";
 import "../../mockData/items.json";
 import { Item } from "@smartcart/shared/src/item";
+import path from 'path';
+
+require('dotenv').config({ path: path.join(__dirname, '../../.env') });
 
 const tagsData: Tag[] = require('../../mockData/tags.json') as Tag[];
 
 async function getTagsFromGpt(products: Item[]): Promise<string> {
+    const url = process.env.MOCK_GPT_URL;
+    if (!url) {
+        throw new Error('MOCK_GPT_URL is not defined');
+    }
     try {
         const productNames = products.map(product => product.correctItemName);
-
         const tagNames = tagsData.map(tag => tag.tagName);
 
-        const response = await fetch('http://localhost:8080/categorize-products', {
+        const response = await fetch(url, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -27,7 +33,6 @@ async function getTagsFromGpt(products: Item[]): Promise<string> {
         }
 
         const responseText = await response.text();
-
         return responseText;
 
     } catch (error) {
@@ -38,7 +43,6 @@ async function getTagsFromGpt(products: Item[]): Promise<string> {
 
 export default async function tagProductsByGPT(products: Item[]): Promise<string> {
     try {
-
         const responseFromGpt = await getTagsFromGpt(products);
         return responseFromGpt;
 
