baa87e2f59fc88de21394c57728e52bb9facd40f
ChavaDrook <c0556761097@gmail.com>
2025-07-30 18:58:42 +0300

    Save uncommitted changes before pull

--- packages/backend/src/db/IRepositories/IPriceRepository.ts
+++ packages/backend/src/db/IRepositories/IPriceRepository.ts
@@ -4,7 +4,12 @@ export interface IPriceRepository {
   addPrice(price: Price): Promise<Price>;
   addManyPrices(prices: Price[]): Promise<Price[]>;
   updatePrice(price: Price): Promise<Price>;
-upsertManyPrices(prices: Price[]): Promise<Price[]>;
+  upsertManyPrices(prices: Price[]): Promise<Price[]>;
   getPriceById(priceId: number): Promise<Price | null>;
   deletePriceById(priceId: number): Promise<void>;
+     // *** הוסף את השורה הבאה ***
+    getAllPrices(): Promise<Price[]>;
+    // *** הוסף גם את זו, שראיתי במימוש ששלחת:
+    getPriceByStorePKItemID(storePK: string, itemId: number): Promise<Price | null>;
+
 }
